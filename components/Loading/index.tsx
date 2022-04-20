7b6b158d88e28e71abac8e1be3e9a280f8ae3ccb
Diogo <diogosantosferreira.01@gmail.com>
2022-04-20 15:17:16 -0300

    feat: image

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -1,12 +1,13 @@
 import styles from './Loading.module.css'
 import Image from 'next/image'
+import img from '../../assets/loading.gif'
 
 const Loading = () => {
 
     return (
         <div className={styles.container}>
             <Image
-                src='./loading.gif'
+                src={img}
                 alt='Loading ..'
                 width={300}
                 height={300}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
+
+const withImages = require('next-images')
+module.exports = withImages({
+ esModule: true
+})
 
 module.exports = nextConfig
